abce55169029674617f801d98b51d9f1be450bb6
Yiukihiro <98154612+Yiukihiro@users.noreply.github.com>
2022-02-01 01:12:39 +0300

    int questionnS + bool + question list + answ.but

---
+++ main app/starter/lib/questions_list.dart
@@ -0,0 +1,15 @@
+
+const questionS = [
+      {
+      'textQuestion':'First Question',
+      'answerButtons' : ['first answer', 'second', 'third', 'forth']
+      },
+      {
+      'textQuestion':'Second Question, press one or more answers or u ll die in ten seconds',
+      'answerButtons' : ['try first', 'try second', 'try third', 'try forth']
+      },
+      {
+      'textQuestion':'Third qestion, try to catch up some movings',
+      'answerButtons' : ['got first', 'got second', 'gor third', 'got forth', 'got fifth']
+      },
+    ];
