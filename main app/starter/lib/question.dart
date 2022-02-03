ae608dbc009a1acd4b3816381298cda0d4a0c7c3
Yiukihiro <butenko.vlad97@gmail.com>
2022-02-03 03:18:11 +0300

    cosmetic

--- main app/starter/lib/question.dart
+++ main app/starter/lib/question.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
   final String questionText;
-  
-   Question(this.questionText);
+
+  Question(this.questionText);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       height: 190,
-      margin:const EdgeInsets.all(40),
+      margin: const EdgeInsets.all(40),
       child: Text(
-      questionText,
-      style: const TextStyle(fontSize: 24),
-      textAlign: TextAlign.center,
-    ),
+        questionText,
+        style: const TextStyle(fontSize: 24),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
